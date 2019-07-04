9967cb3ae5b54e5f61fe9a05b38fe95c86c21365
viorelaBudianu <viorela.hariga@yahoo.com>
2019-07-04 16:59:13 +0300

    Calculator Dynamic + Calculator Generic.Math

---
+++ SpecialString/Calculator/CalculatorBonus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorBonus<T>
+    {
+        public static T Add(T a, T b )
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(dynamic)(a1 + b1);
+        }
+        public static T Sub(T a, T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(dynamic)(a1 - b1);
+        }
+        public static T Multiplication(T a, T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(dynamic)(a1 * b1);
+        }
+        public static T Division(T a, T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            if (b1 == 0 || b1 == null)
+                throw new ArithmeticException("Cannot make Division on a null/0 value");
+
+            return (T)(dynamic)(a1 / b1);
+        }
+    }
+}
---
+++ SpecialString/CalculatorBonusNuget/CalculatorGenericMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generic.Math;
+namespace CalculatorBonusNuget
+{
+    public class CalculatorGenericMath
+    {
+        public static T Add<T>(T value1, T value2)
+        {
+            return GenericMath<T>.Add(value1, value2);
+        }
+        public static T Subtract<T>(T value1, T value2)
+        {
+            return GenericMath<T>.Subtract(value1, value2);
+        }
+
+        public static T Multiply<T>(T value1, T value2)
+        {
+            return GenericMath<T>.Multiply(value1, value2);
+        }
+
+        public static T Divide<T>(T value1, T value2)
+        {
+            return GenericMath<T>.Divide(value1, value2);
+        }
+    }
+}
